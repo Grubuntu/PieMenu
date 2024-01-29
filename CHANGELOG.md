21ec466ccdc3adc53119ed573fb75fb0afe82d75
hasecilu <hasecilu@tuta.io>
2024-01-29 16:07:29 -0600

    Move changelog to markdown file

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+# CHANGELOG
+
+### 1.3.4
+
+- Move globals settings on a new tab
+- Added setting for show or not the QuickMenu
+- Added checkbox in parameters for Contextual activation
+- Set default theme (Legacy) on a new installation
+- Update stylesheets for #styleButtonMenu::menu-indicator (setting for QuickMenu)
+
+### 1.3.5
+
+- Factorization and clean some code
+- Added differents shapes for PieMenus
+- Added possibility to display the command name in the menu (only Pie shape)
+- Added a button with information about developers and licence (Pgilfernandez)
+
+### 1.3.6
+
+- Update layout of PieMenu Preferences for better readability (Pgilfernandez)
+- Fix some typos (Pgilfernandez)
+- Added some styles for QLabel command name (Pgilfernandez)
+- Added "LeftRight" shape
+- Fix problem with ghosting when reload workbenches
--- InitGui.py
+++ InitGui.py
@@ -25,28 +25,6 @@
 # http://forum.freecadweb.org/
 # http://www.freecadweb.org/wiki/index.php?title=Code_snippets
 
-# Changelog :
-#
-# 1.3.4
-# Move globals settings on a new tab
-# Added setting for show or not the QuickMenu
-# Added checkbox in parameters for Contextual activation
-# Set default theme (Legacy) on a new installation
-# Update stylesheets for #styleButtonMenu::menu-indicator (setting for QuickMenu)
-#
-# 1.3.5
-# Factorization and clean some code
-# Added differents shapes for PieMenus
-# Added possibility to display the command name in the menu (only Pie shape)
-# Added a button with information about developers and licence (Pgilfernandez)
-#
-# 1.3.6
-# Update layout of PieMenu Preferences for better readability (Pgilfernandez)
-# Fix some typos (Pgilfernandez)
-# Added some styles for QLabel command name (Pgilfernandez)
-# Added "LeftRight" shape
-# Fix problem with ghosting when reload workbenches
-
 
 global PIE_MENU_VERSION
 PIE_MENU_VERSION = "1.3.6"
